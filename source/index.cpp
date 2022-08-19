8f7ac9cd4a13dfc3da459178037828b3f1cd4a38
Kambiz Asadzadeh <kambiz.ceo@gmail.com>
2022-08-19 10:40:17 +0430

    Update index.cpp

--- source/index.cpp
+++ source/index.cpp
@@ -1,9 +1,11 @@
 #include "index.hpp"
 #include "core/core.hpp"
+#include "core/database.hpp"
 
 //!Tegra
 TEGRA_USING_NAMESPACE Tegra;
 TEGRA_USING_NAMESPACE Tegra::CMS;
+TEGRA_USING_NAMESPACE Tegra::Database;
 TEGRA_USING_NAMESPACE Framework;
 
 TEGRA_NAMESPACE_BEGIN(Pages)
@@ -11,12 +13,18 @@ TEGRA_NAMESPACE_BEGIN(Pages)
 void Home::index(const Framework::HttpRequestPtr& req, std::function<void(const HttpResponsePtr &)>&& callback) const
 {
     Scope<ApplicationData> appDataPtr(new ApplicationData());
+    {
+        appDataPtr->path    = req->getPath();
+        appDataPtr->module  = "index";
+    }
     Scope<Multilangual::Language> languagePtr(new Multilangual::Language(appDataPtr->path.value()));
-    appDataPtr->path    = req->getPath();
-    appDataPtr->module  = "index";
+
     auto resp = HttpResponse::newHttpResponse();
-    std::string message = "Hello, I am " + TEGRA_TRANSLATOR("global","name");
+
+    std::string message{TEGRA_RUNTIME_FORMAT(TEGRA_TRANSLATOR("global", "greeting"), TEGRA_TRANSLATOR("global", "name"))};
+
     resp->setBody(message);
+
     callback(resp);
 }
 
